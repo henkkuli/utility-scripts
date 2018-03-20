b2eb07e841bd6b10f5fe2ff5ae9353f24f899320
Henrik Lievonen <henrik.lievonen@hotmail.com>
2018-03-20 22:55:06 +0200

    Size parameter and locate legend

--- matlab/save_fig.m
+++ matlab/save_fig.m
@@ -1,10 +1,22 @@
-function [] = save_fig(filename, fig)
+function [] = save_fig(filename, fig, size)
 %SAVE_PLOT Save plot to a file with name filename.
+
+    if nargin < 3
+        size = [6 3];
+    end
 
     % Set paper size
     fig.PaperUnits = 'inches';
-    fig.PaperPosition = [0 0 6 3];
-    fig.PaperSize = [6 3];
+    fig.PaperPosition = [0 0 size];
+    fig.PaperSize = [size];
+
+    % Move the legend to the best location
+    leg = findobj(fig, 'Type', 'Legend');
+    if ~isempty(leg)
+        drawnow;
+        leg.Location = 'Best';
+    end
+
     % Wait for the figure to render
     drawnow;
     % And print to the file
